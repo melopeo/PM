2da8d686f00077889fc8bd65d2609101ffa4ebb9
Pedro Mercado <34275963+melopeo@users.noreply.github.com>
2018-06-25 12:40:22 +0200

    Update experiments_main.m

--- experiments_main.m
+++ experiments_main.m
@@ -1,6 +1,6 @@
 function experiments_main
 
-% Calles scripts corresponding to different numerical evaluations of our
+% Calls scripts corresponding to different numerical evaluations of our
 % paper
 
 SBM_case_1           % section 3.1 of our paper
